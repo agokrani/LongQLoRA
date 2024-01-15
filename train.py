34cdacdd7596d01313edf58bd018bd1937b40d5b
Aman Gokrani <amaan.gokrani@gmail.com>
2024-01-15 17:06:18 +0100

    phi2 config moved and datasets ready for training

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ from transformers import (
     AutoConfig
 )
 import argparse
-from loguru import logger
+import logging
 import os
 from os.path import join
 import yaml
@@ -24,7 +24,7 @@ from component.trainer import LoRATrainer
 from component.loss import CausalLMLoss
 from attention.llama_attn_replace import replace_llama_attn
 
-
+logger = logging.getLogger(__name__)
 def verify_model_dtype(model):
     """
     查看模型种各种类型的参数的情况
@@ -218,6 +218,7 @@ def init_components(args, training_args):
     training_args.ddp_find_unused_parameters = False
     # 加载model和tokenizer
     model, tokenizer = load_model_and_tokenizer(args, training_args)
+    import pdb;pdb.set_trace()
     # 插入adapter
     model = insert_adapter(args, model)
     # 初始化损失函数
